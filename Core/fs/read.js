70002c782b44bba1778ed5dc1609c217d7d274c7
NightDev19 <sherwintajan143@gmail.com>
2025-11-11 16:52:12 +0800

    feat(Intro to NodeJS) : Updated -> fs -> read

--- Core/fs/read.js
+++ Core/fs/read.js
@@ -1,12 +1,31 @@
-// to read a file in node.js we need to use the fs module
-const fs = require("fs");
+// Import the fs module for callback and sync methods
+import fs from "node:fs";
+// Import fs/promises for promise-based methods
+import fsPromises from "node:fs/promises";
 
-// to read a file in node.js we need to use the fs module
-fs.readFile(
-  "/home/night/NightDev/NodeJs/Introduction/mini_project.js",
-  "utf8", /* encoding to show the content of the file */
-  (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  },
-);
+const file = "/home/night/NightDev/NodeJs/Introduction/mini_project.js";
+
+// --- 1. Callback-based async read ---
+fs.readFile(file, "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading file (callback):", err);
+    return;
+  }
+  console.log("Callback read:\n", data);
+});
+
+// --- 2. Synchronous read ---
+try {
+  const data = fs.readFileSync(file, "utf8");
+  console.log("Sync read:\n", data);
+} catch (err) {
+  console.error("Error reading file (sync):", err);
+}
+
+// --- 3. Promise-based async read ---
+try {
+  const data = await fsPromises.readFile(file, { encoding: "utf8" });
+  console.log("Promise read:\n", data);
+} catch (err) {
+  console.error("Error reading file (promise):", err);
+}
